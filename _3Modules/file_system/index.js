dfe7ff20434cdccebbcef68072f989f52abe95e2
mdriyadtalukder <riyadtalukder67@gmail.com>
2025-10-25 19:57:36 +0600

    updated

---
+++ _3Modules/file_system/index.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const path = require("path");
+
+//synchronous way to creating file
+const dataFolder = path.join(__dirname, "data");
+if (!fs.existsSync(dataFolder)) {
+  fs.mkdirSync(dataFolder);
+  console.log("folder is created!");
+}
+
+const filePath = path.join(dataFolder, "example.txt");
+fs.writeFileSync(filePath, "hello , world");
+console.log("file is created!");
+
+const readFile = fs.readFileSync(filePath, "utf8");
+console.log(readFile);
+fs.appendFileSync(filePath, "\nhow are u?");
+const readFile2 = fs.readFileSync(filePath, "utf8");
+console.log(readFile2);
+
+//asynchronous way to creating file
+const asyncFilePath = path.join(dataFolder, "async.txt");
+fs.writeFile(asyncFilePath, "asyc created", (err, data) => {
+  if (err) throw err;
+  console.log("Async file is created!");
+
+  fs.readFile(asyncFilePath, "utf8", (err, data) => {
+    if (err) throw err;
+    console.log(data);
+
+    fs.appendFile(asyncFilePath, "\nupdated file", (err, data) => {
+      if (err) throw err;
+      console.log("file created");
+      fs.readFile(asyncFilePath, "utf8", (data, updatedData) => {
+        if (err) throw err;
+        console.log(updatedData);
+      });
+    });
+  });
+});
